b18accb7c42a63c6d5caf410794474fefa34529c
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-09-06 18:04:53 -0600

    delete route

--- server.js
+++ server.js
@@ -12,6 +12,59 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// app.get("/api", (req, res) => {
+//   res.json({
+//     title: "test note",
+//     text: "taking my first note",
+//     note_id: randomUUID(),
+//   });
+// });
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile("db/db.json", "utf-8", (err, data) => {
+    return err ? console.log(err) : res.json(JSON.parse(data));
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  const { title, text } = req.body;
+  if (title && text) {
+    fs.readFile("db/db.json", "utf-8", (err, data) => {
+      if (err) console.log(err);
+      const notes = JSON.parse(data);
+      const newNote = {
+        title,
+        text,
+        id: randomUUID(),
+      };
+      console.log(data);
+      notes.push(newNote);
+
+      fs.writeFile(`./db/db.json`, JSON.stringify(notes), (err) => {
+        if (err) console.error(err);
+        res.redirect("/notes");
+      });
+    });
+  }
+});
+// redirect
+
+app.delete("/api/notes/:id", (req, res) => {
+  console.log(req.params);
+  fs.readFile("db/db.json", "utf-8", (err, data) => {
+    if (err) console.log(err);
+    // console.log(data);
+    const notes = JSON.parse(data);
+    const filtered = notes.filter((note) => note.id !== req.params.id);
+    // console.log(filtered);
+    fs.writeFile("db/db.json", JSON.stringify(filtered), (err) => {
+      if (err) console.log(err);
+      res.redirect("/notes");
+    });
+  });
+});
+
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
@@ -19,44 +72,6 @@ app.get("/", (req, res) =>
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
-app.get("/api", (req, res) => {
-  res.json({
-    title: "test note",
-    text: "taking my first note",
-    note_id: randomUUID(),
-  });
-});
-
-app.get("/api/notes", (req, res) => res.json(db));
-
-app.post("/api/notes", (req, res) => {
-  console.info(`${req.method} request received to add a note`);
-  const { title, text } = req.body;
-  if (title && text) {
-    const newNote = {
-      title,
-      text,
-      note_id: randomUUID(),
-    };
-    const noteString = JSON.stringify(newNote);
-
-    fs.writeFile(`./db/${newNote.title}.json`, noteString, (err) =>
-      err
-        ? console.error(err)
-        : console.log(`Note for ${newNote.title} has been written to JSON file`)
-    );
-
-    const response = {
-      status: "success",
-      body: newNote,
-    };
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-    res.status(500).json("Error in posting note");
-  }
-});
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 app.listen(PORT, () =>
   console.log(`Note app listening at http://localhost:${PORT}`)
